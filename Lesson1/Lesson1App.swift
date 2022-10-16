ada8b1789e7ee3c820435a939b65d7289165d7b9
Nikita Shirobokov <Nik>
2022-10-16 16:18:23 +0400

    Initial Commit

---
+++ Lesson1/Lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1App.swift
+//  Lesson1
+//
+//  Created by Nikita Shirobokov on 16.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
